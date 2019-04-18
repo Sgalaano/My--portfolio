95c288e313e681a64fef6e69707617a7c45af0c2
nignanthomas <nignanthomas@gmail.com>
2019-04-18 08:20:52 +0300

    add back to top button

--- js/main.js
+++ js/main.js
@@ -65,6 +65,26 @@
 
     };
 
+    /* Back to Top
+    * ------------------------------------------------------ */
+    var clBackToTop = function() {
+
+        var pxShow  = 500,         // height on which the button will show
+        fadeInTime  = 400,         // how slow/fast you want the button to show
+        fadeOutTime = 400,         // how slow/fast you want the button to hide
+        scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+        goTopButton = $(".go-top")
+
+        // Show or hide the sticky footer button
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() >= pxShow) {
+                goTopButton.fadeIn(fadeInTime);
+            } else {
+                goTopButton.fadeOut(fadeOutTime);
+            }
+        });
+    };
+
 
     /* Initialize
     * ------------------------------------------------------ */
@@ -72,6 +92,7 @@
 
         clMenuOnScrolldown();
         clOffCanvas();
+        clBackToTop();
 
     })();
 
